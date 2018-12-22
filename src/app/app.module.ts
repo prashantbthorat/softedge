d15c253f12b5e27b02b2a8d07d5a25f6c0b0dc7d
admin <admin@SOFTEDGEIT.COM>
2018-12-22 09:26:11 +0530

    Working on signin componant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule,FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TopPanelComponent } from './comman/top-panel/top-panel.component';
@@ -41,6 +42,8 @@ const appRoutes: Routes = [
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
+    FormsModule,
     RouterModule.forRoot(
       appRoutes
     )
--- src/app/auth/sign-in/sign-in.component.ts
+++ src/app/auth/sign-in/sign-in.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+
+  constructor() { 
+  }
 
   ngOnInit() {
   }
 
+  signInSubmit() {
+  }
+  
 }
